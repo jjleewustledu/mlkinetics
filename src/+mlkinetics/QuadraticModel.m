11ca154387cde02da090fad2d57a6d4b35d4f22f
John Lee <16329959+jjleewustledu@users.noreply.github.com>
2023-11-24 16:00:11 -0600

    refactor mixTacAif(), including use of t0, also tF

--- src/+mlkinetics/QuadraticModel.m
+++ src/+mlkinetics/QuadraticModel.m
@@ -81,10 +81,10 @@ classdef (Abstract) QuadraticModel < handle & mlkinetics.Model
             N = length(ipr.aif);
             M = length(ipr.f);
             times = 0:(N-1);
-            rho = zeros(M, this.tF_-this.t0_+1);
+            rho = zeros(M, this.tF-this.t0+1);
             for r = 1:M
                 rho_ = ipr.f(r)*conv(ipr.aif, exp(-(ipr.f(r)/lam + alph)*times));
-                rho(r,:) = rho_(this.t0_+1:this.tF_+1);
+                rho(r,:) = rho_(this.t0+1:this.tF+1);
             end
 
             obs = trapz(rho, 2); % \int dt
@@ -99,32 +99,32 @@ classdef (Abstract) QuadraticModel < handle & mlkinetics.Model
             
             ip = inputParser;
             addRequired(ip, 'tac', @isnumeric)
-            addParameter(ip, 'timesMid', this.timesMid_, @isnumeric)
-            addParameter(ip, 't0', this.t0_, @isscalar)
-            addParameter(ip, 'tF', this.tF_, @isscalar)
+            addParameter(ip, 'times_sampled', this.times_sampled, @isnumeric)
+            addParameter(ip, 't0', this.t0, @isscalar)
+            addParameter(ip, 'tF', this.tF, @isscalar)
             parse(ip, varargin{:})
             ipr = ip.Results;            
-            assert(size(ipr.tac,4) == length(ipr.timesMid))
+            assert(size(ipr.tac,4) == length(ipr.times_sampled))
 
             if size(ipr.tac,4) < 10
-                timesMore = linspace(ipr.timesMid(1), ipr.timesMid(end)); % N = 100
-                ipr.tac = this.interpolate_img(ipr.tac, ipr.timesMid, timesMore);
+                times_more = linspace(ipr.times_sampled(1), ipr.times_sampled(end)); % N = 100
+                ipr.tac = this.interpolate_img(ipr.tac, ipr.times_sampled, times_more);
             else
-                timesMore = ipr.timesMid;
+                times_more = ipr.times_sampled;
             end            
-            window = ipr.t0 <= timesMore & timesMore <= ipr.tF;            
-            obs = trapz(timesMore(window), ipr.tac(:,:,:,window), 4);
+            window = ipr.t0 <= times_more & times_more <= ipr.tF;            
+            obs = trapz(times_more(window), ipr.tac(:,:,:,window), 4);
         end
     end
 
     %% PROTECTED
 
     methods (Access = protected)
-        function img = interpolate_img(~, img, timesMid, timesMore)
+        function img = interpolate_img(~, img, times_sampled, times_more)
             sz = size(img);
             %tic
-            [x,y,z,t] = ndgrid(1:sz(1), 1:sz(2), 1:sz(3), timesMid);
-            [xq,yq,zq,tq] = ndgrid(1:sz(1), 1:sz(2), 1:sz(3), timesMore);
+            [x,y,z,t] = ndgrid(1:sz(1), 1:sz(2), 1:sz(3), times_sampled);
+            [xq,yq,zq,tq] = ndgrid(1:sz(1), 1:sz(2), 1:sz(3), times_more);
             img = interpn(x, y, z, t, img, xq, yq, zq, tq);
             %toc
         end
