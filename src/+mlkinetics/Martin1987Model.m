9189144790410310757e3c1f040db4f78fc5a08d
John Lee <16329959+jjleewustledu@users.noreply.github.com>
2023-11-27 14:41:10 -0600

    increasing symmetries of tissue-compartment models, even when not subclassing; Martin is outlier

--- src/+mlkinetics/Martin1987Model.m
+++ src/+mlkinetics/Martin1987Model.m
@@ -1,4 +1,4 @@
-classdef Martin1987Model < handle & mlkinetics.Model
+classdef Martin1987Model < handle & mlkinetics.QuadraticModel
     %% line1
     %  line2
     %  
@@ -6,85 +6,75 @@ classdef Martin1987Model < handle & mlkinetics.Model
     %  Developed on Matlab 9.14.0.2306882 (R2023a) Update 4 for MACI64.  Copyright 2023 John J. Lee.
     
     properties (Dependent)
-        dlicv_ic
+        co_ic
     end
 
     methods %% GET
-        function g = get.dlicv_ic(this)
-            g = this.bids_med_.dlicv.ic;
+        function g = get.co_ic(this)
+            if ~isempty(this.co_ic_)
+                g = copy(this.co_ic_);
+                return
+            end
+            this.co_ic_ = this.scanner_kit_.do_make_activity_density();
+            g = copy(this.co_ic_);
         end
     end
 
     methods
-        function v1_ = make_solution(this, varargin)
-            %  Return v1_ in R^, mlfourd.ImagingContext2, without saving to filesystems.  
+        function soln = build_solution(this, varargin)
+
+            % check dynamic imaging
+
+            % check & adjust input functions, esp. their timings and timing boundaries, 
+            % to match dynamic imaging 
+
+            obsPet = this.obsFromTac(this.measurement_, t0=this.t0, tF=this.tF);
+            integralAif = trapz(this.artery_interpolated_(this.t0+1:this.tF+1));
+            c1 = mlkinetics.OxyMetabConversion.RATIO_SMALL_LARGE_HCT;
+            c2 = mlkinetics.OxyMetabConversion.DENSITY_BRAIN;
+            img = obsPet/(c1*c2*integralAif);
+            img = mlkinetics.OxyMetabConversion.v1ToCbv(img);
+            img(img < 0) = 0;
+            img(img > 100) = 100;
             
-            scanPath = this.bids_med_.scanPath;
-            ensuredir(scanPath);
-            pwd0 = pushd(scanPath);
-            
-            msk = this.dlicv_ic;
-            v1_ = msk.nifti;
-            v1_.filepath = scanPath;
-            obj = this.vsOnAtlas(tags=this.tags);
-            v1_.fileprefix = obj.fileprefix;
-
-            % solve Martin
-            this.solver_ = this.solver_.solve();  
-
-            % insert Martin solutions into fs
-            v1_.img = this.v1('typ', 'single');
-            v1_ = mlfourd.ImagingContext2(v1_);
-
-            popd(pwd0);
+            soln = copy(this.co_ic.imagingFormat);
+            soln.img = single(img);
+            soln.fileprefix = strrep(strrep(this.co_ic.fileprefix, "_trc-co", "_cbv"), "_trc-oc", "_cbv");
+            soln = mlfourd.ImagingContext2(soln);
+            this.product_ = soln; % fullfills mlkinetics.Model's builder design pattern
         end
+        function ks_ = ks(this, varargin)
+            %% ks == v1, to facilitate overloading
+
+            ks_ = this.product;
+        end 
+        function t = tauObs(~)
+            t = 240;
+        end
+        function t = timeStar(~)
+            t = 60; % sec, per Martin's 1987 paper
+        end
+        function v1_ = v1(this)
+            v1_ = this.product;
+        end 
     end
 
     methods (Static)
         function this = create(varargin)
             this = mlkinetics.Martin1987Model(varargin{:});
-
-            [this.measurement_,this.times_sampled_,this.t0_,this.artery_interpolated_] = this.mixTacAif();
         end
     end
 
-    %% PRIVATE
+    %% PROTECTED
 
-    properties (Access = private)
-        bids_med_
+    properties (Access = protected)
+        co_ic_
     end
 
-    methods (Access = private)
+    methods (Access = protected)
         function this = Martin1987Model(varargin)
-            this = this@mlkinetics.Model(varargin{:});
+            this = this@mlkinetics.QuadraticModel(varargin{:});
         end
-
-        function ks_ = ks(this, varargin)
-            %% ks == v1
-            %  @param 'typ' is char, understood by imagingType.            
-            
-            ip = inputParser;
-            ip.KeepUnmatched = true;
-            addParameter(ip, 'typ', 'mlfourd.ImagingContext2', @ischar)
-            parse(ip, varargin{:})
-            ipr = ip.Results;
-            
-            k(1) = k1(this.strategy_, varargin{:});
-             
-            roibin = logical(this.roi);
-            ks_ = copy(this.roi.fourdfp);
-            ks_.img = zeros([size(this.roi) this.LENK]);
-            for t = 1:length(k)
-                img = zeros(size(this.roi), 'single');
-                img(roibin) = k(t);
-                ks_.img(:,:,:,t) = img;
-            end
-            ks_.fileprefix = this.sessionData.vsOnAtlas('typ', 'fp', 'tags', [this.blurTag this.regionTag]);
-            ks_ = imagingType(ipr.typ, ks_);
-        end 
-        function v1_ = v1(this, varargin)
-            v1_ = this.ks(varargin{:});
-        end 
     end
     
     %  Created with mlsystem.Newcl, inspired by Frank Gonzalez-Morphy's newfcn.
