64719c3e4aedc900734e44f1e285e145c34c4e0e
John Lee <16329959+jjleewustledu@users.noreply.github.com>
2023-11-27 14:38:49 -0600

    TCModel gathers tissue-compartment commonalities

--- src/+mlkinetics/TCModel.m
+++ src/+mlkinetics/TCModel.m
@@ -1,88 +1,70 @@
-classdef TCModel < handle & mlkinetics.Model
+classdef (Abstract) TCModel < handle & mlkinetics.Model
     %% line1
     %  line2
     %  
     %  Created 11-Oct-2023 01:33:12 by jjlee in repository /Users/jjlee/MATLAB-Drive/mlkinetics/src/+mlkinetics.
     %  Developed on Matlab 23.2.0.2380103 (R2023b) Update 1 for MACI64.  Copyright 2023 John J. Lee.
     
-    properties
-        LENK
-        mgdL_to_mmolL
+    properties (Abstract, Constant)
+        knames
+    end
 
-        artery_interpolated
+    methods (Abstract)
+        sampled(this)
+    end
+
+    properties
+        Data
+        LENK
         map
-        times_sampled
     end
 
     properties (Dependent)
-        bids_med
-        parc
-        trc_ic % activityDensity from ScannerKit
+        raichleks_ic
+        martinv1_ic
     end
 
     methods %% GET, SET
-        function g = get.bids_med(this)
-            if ~isempty(this.bids_med_)
-                g = this.bids_med_;
+        function g = get.raichleks_ic(this)
+            if isfield(this.data, "raichleks_ic")
+                g = this.data.raichleks_ic;
                 return
             end
-
-            this.bids_med_ = this.bids_kit_.make_bids_med();
-            g = this.bids_med_;
+            if isfield(this.data, "cbf_ic")
+                g = this.data.cbf_ic;
+                g = mlkinetics.OxyMetabConversion.f1ToCbf(g);
+                return
+            end
+            g = [];
         end
-        function g = get.parc(this)
-            if ~isempty(this.parc_)
-                g = this.parc_;
+        function g = get.martinv1_ic(this)
+            if isfield(this.data, "martinv1_ic")
+                g = this.data.martinv1_ic;
                 return
             end
-
-            this.parc_ = this.parc_kit_.make_parc();
-            g = this.parc_;
-        end
-        function g = get.trc_ic(this)
-            if ~isempty(this.trc_ic_)
-                g = copy(this.trc_ic_);
+            if isfield(this.data, "cbv_ic")
+                g = this.data.cbv_ic;
+                g = mlkinetics.OxyMetabConversion.cbvToV1(g);
                 return
             end
-
-            this.trc_ic_ = this.scanner_kit_.do_make_activity_density(decayCorrected=true);
-            g = copy(this.trc_ic_);
-        end
-
-        function this = set_times_sampled(this, s)
-            if isempty(s)
-                return
-            end
-            this.times_sampled = s;
-        end
-        function this = set_artery_interpolated(this, s)
-            if isempty(s)
-                return
-            end
-            % artery_interpolated may be shorter than scanner times_sampled
-            assert(~isempty(this.times_sampled))
-            tBuffer = 0;
-            if length(s) ~= floor(this.times_sampled(end)) + tBuffer + 1
-                this.artery_interpolated = ...
-                    interp1(-tBuffer:(length(s)-tBuffer-1), s, -tBuffer:this.times_sampled(end), ...
-                    'linear', 0);
-                return
-            end
-            this.artery_interpolated = s;
+            g = [];
         end
     end
 
     methods
+        function s = fqfp(this, opts)
+            arguments
+                this mlkinetics.TCModel
+                opts.tag {mustBeTextScalar} = "ks"
+            end
+
+            s = this.product.fqfp;
+            re = regexp(s, "\S+_(?<trc>trc-\w+)_\S+", "names");
+            strrep(s, re.trc, opts.tag)
+        end
     end
 
     %% PROTECTED
-
-    properties (Access = protected)
-        bids_med_
-        parc_
-        solver_
-        trc_ic_
-    end
 
     methods (Access = protected)
         function this = TCModel(varargin)
